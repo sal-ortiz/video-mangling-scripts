0b162c2c7399796cedef0f73d6338ccf6f1b8a4a
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2024-10-03 22:14:14 -0500

    added windows scripts to handle title/comments

---
+++ info.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+::setlocal EnableExtensions EnableDelayedExpansion
+::title scaling down and compressing video
+
+set BIN_PATH=.\bin
+
+set INP_FILE=%~1
+
+if "%INP_FILE%" == "" (
+  echo "%0 <input>"
+
+  exit /b 1
+)
+
+
+::%BIN_PATH%\ffprobe -loglevel error -show_entries stream_tags:format_tags -of json "%INP_FILE%"
+%BIN_PATH%\exiftool -*Title* -*Comment* "%INP_FILE%"
+
+exit /b 0
---
+++ strip.cmd
@@ -0,0 +1,33 @@
+@echo off
+
+::setlocal EnableExtensions EnableDelayedExpansion
+::title scaling down and compressing video
+
+set TMP_PATH=.\tmp
+set BIN_PATH=.\bin
+
+set INP_FILE=%~1
+set OUTP_FILE=%~2
+
+set TMP_FILE=%TMP_PATH%\%INP_FILE%.tmp
+
+if "%INP_FILE%" == "" (
+  echo "%0 <input> [output]"
+
+  exit /b 1
+)
+
+if "%OUTP_FILE%" == "" (
+  set OUTP_FILE=%INP_FILE%
+)
+
+
+copy /y /v /d "%INP_FILE%" "%TMP_FILE%" > nul
+
+::%BIN_PATH%\ffmpeg -y -i "%TMP_FILE%" v -map_metadata -1 -c:v copy -c:a copy "%OUTP_FILE%"
+%BIN_PATH%\exiftool -r -overwrite_original -title= -comment= -subtitle= "%TMP_FILE%"
+
+copy /v /y "%TMP_FILE%" "%OUTP_FILE%"
+del /q /f "%TMP_FILE%"
+
+exit /b 0
