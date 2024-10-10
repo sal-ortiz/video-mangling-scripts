99e27ece4e45d7f1cd31b33e862d5fc95c2988ca
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2024-10-10 18:07:51 -0500

    added bash script to extract audio from video

---
+++ extract_audio.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+APP_ROOT=$( readlink --canonicalize $( dirname $0 ) )
+USER_PATH=$( pwd )
+
+BIN_PATH=$APP_ROOT/bin
+
+INP_FILE=$1
+OUTP_FILE=$2
+
+if [ -z $INP_FILE ]; then
+  echo "$( basename $0 ) <input> <output>"
+  exit 1
+fi
+
+if [ -z $OUTP_FILE ]; then
+  OUTP_FILE=$INP_FILE.mp3
+fi
+
+$BIN_PATH/ffmpeg -i $INP_FILE -f mp3 -ab 192000 -vn $OUTP_FILE
+
+exit 0
