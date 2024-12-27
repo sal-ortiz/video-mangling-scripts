4d344649ffd600db7af957604163cf2cd8ce08c3
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2024-12-26 20:18:34 -0600

    cleaned up filename handling in sh script

--- shrink.sh
+++ shrink.sh
@@ -9,26 +9,25 @@ BIN_PATH=$APP_ROOT/bin
 INP_FILE=$1
 OUTP_FILE=$2
 
-TMP_FILE=$TMP_PATH/$INP_FILE.tmp
-
 # the lower the value the higher the bitrate (24~30 is a good medium)
-CRF_VAL=20
+CRF_VAL=33
 
-if [ -z $INP_FILE ]; then
+if [ -z "$INP_FILE" ]; then
   echo "$( basename $0 ) <input> [output]"
 
   exit 1
 fi
 
-if [ -z $OUTP_FILE ]; then
+if [ -z "$OUTP_FILE" ]; then
   OUTP_FILE=$INP_FILE
 fi
 
+TMP_FILE=$( readlink --canonicalize "$TMP_PATH/$( basename "$INP_FILE" ).tmp" )
 
-cp -fv $INP_FILE $TMP_FILE
+cp -fv "$( readlink --canonicalize "$USER_PATH/$INP_FILE" )" "$TMP_FILE"
 
-$BIN_PATH/ffmpeg -y -i $TMP_FILE -vcodec libx265 -crf $CRF_VAL $OUTP_FILE
+"$BIN_PATH/ffmpeg" -y -i "$TMP_FILE" -vcodec libx265 -crf $CRF_VAL "$( readlink --canonicalize "$USER_PATH/$OUTP_FILE" )"
 
-rm -fv $TMP_FILE
+rm -fv "$TMP_FILE"
 
 exit 0
