c3a39a0263927cee2795b9e1ccca491ad6c9d53a
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2025-05-26 12:49:16 -0500

    allow meta strip script to handle spaces in names

--- strip.sh
+++ strip.sh
@@ -9,24 +9,24 @@ BIN_PATH=$APP_ROOT/bin
 INP_FILE=$1
 OUTP_FILE=$2
 
-if [ -z $INP_FILE ]; then
+if [ -z "$INP_FILE" ]; then
   echo "$( basename $0 ) <input> [output]"
 
   exit 1
 fi
 
-TMP_FILE=$TMP_PATH/$( basename $INP_FILE ).tmp
+TMP_FILE=$TMP_PATH/$( basename "$INP_FILE" ).tmp
 
 if [ -z $OUTP_FILE ]; then
   OUTP_FILE=$INP_FILE
 fi
 
 
-cp -fv $INP_FILE $TMP_FILE
+cp -fv "$INP_FILE" "$TMP_FILE"
 
 #$BIN_PATH/exiftool -r -overwrite_original -title= -comment= -subtitle= -artist= -album= $TMP_FILE
-$BIN_PATH/exiftool -r -overwrite_original -all= $TMP_FILE
+$BIN_PATH/exiftool -r -overwrite_original -all= "$TMP_FILE"
 
-mv -f $TMP_FILE $OUTP_FILE
+mv -fv "$TMP_FILE" "$OUTP_FILE"
 
 exit 0
