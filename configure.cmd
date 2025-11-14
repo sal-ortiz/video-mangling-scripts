0b60840b21ac92d1feb8283a76ed7c75d702860a
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2025-11-14 11:02:52 -0600

    updated version of exiftools in windows script

--- configure.cmd
+++ configure.cmd
@@ -7,7 +7,7 @@ set TMP_PATH=.\tmp
 set BIN_PATH=.\bin
 
 set FFMPEG_URL=https://github.com/GyanD/codexffmpeg/releases/download/7.1/ffmpeg-7.1-full_build-shared.zip
-set EXIFTOOL_URL=https://exiftool.org/exiftool-12.97_64.zip
+set EXIFTOOL_URL=https://exiftool.org/exiftool-13.41_64.zip
 
 if not exist %TMP_PATH%\ (
   mkdir %TMP_PATH%
@@ -25,7 +25,7 @@ powershell Invoke-WebRequest %EXIFTOOL_URL% -OutFile %TMP_PATH%\exiftool.zip
 powershell Expand-Archive %TMP_PATH%\exiftool.zip -DestinationPath %TMP_PATH%\
 
 copy /v /y %TMP_PATH%\ffmpeg-7.1-full_build-shared\bin\* %BIN_PATH%\
-xcopy /v /y /e %TMP_PATH%\exiftool-12.97_64\* %BIN_PATH%\
+xcopy /v /y /e %TMP_PATH%\exiftool-13.41_64\* %BIN_PATH%\
 
 rename "%BIN_PATH%\exiftool(-k).exe" exiftool.exe
 
