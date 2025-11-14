7ae54339f53d450c04fe7ef2fa4a8d9e9bf85796
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2025-11-14 14:41:28 -0600

    store exiftool version in a variable for quick edits

--- configure.cmd
+++ configure.cmd
@@ -3,11 +3,13 @@
 ::setlocal EnableExtensions EnableDelayedExpansion
 ::title scaling down and compressing video
 
+set EXIF_VER=13.41_64
+
 set TMP_PATH=.\tmp
 set BIN_PATH=.\bin
 
 set FFMPEG_URL=https://github.com/GyanD/codexffmpeg/releases/download/7.1/ffmpeg-7.1-full_build-shared.zip
-set EXIFTOOL_URL=https://exiftool.org/exiftool-13.41_64.zip
+set EXIFTOOL_URL=https://exiftool.org/exiftool-%EXIF_VER%.zip
 
 if not exist %TMP_PATH%\ (
   mkdir %TMP_PATH%
@@ -25,7 +27,7 @@ powershell Invoke-WebRequest %EXIFTOOL_URL% -OutFile %TMP_PATH%\exiftool.zip
 powershell Expand-Archive %TMP_PATH%\exiftool.zip -DestinationPath %TMP_PATH%\
 
 copy /v /y %TMP_PATH%\ffmpeg-7.1-full_build-shared\bin\* %BIN_PATH%\
-xcopy /v /y /e %TMP_PATH%\exiftool-13.41_64\* %BIN_PATH%\
+xcopy /v /y /e %TMP_PATH%\exiftool-%EXIF_VER%\* %BIN_PATH%\
 
 rename "%BIN_PATH%\exiftool(-k).exe" exiftool.exe
 
