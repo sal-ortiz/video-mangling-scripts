b11ae6b09d0b1516512394353732c6e5ff802553
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2025-11-14 14:35:53 -0600

    store exiftool version in a variable for quick edits

--- configure.sh
+++ configure.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+EXIF_VER=13.40
+
 APP_ROOT=$( readlink --canonicalize $( dirname $0 ) )
 USER_PATH=$( pwd )
 
@@ -7,7 +9,7 @@ TMP_PATH=$APP_ROOT/tmp
 BIN_PATH=$APP_ROOT/bin
 
 FFMPEG_URL=https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
-EXIFTOOL_URL=https://sourceforge.net/projects/exiftool/files/Image-ExifTool-13.40.tar.gz
+EXIFTOOL_URL=https://sourceforge.net/projects/exiftool/files/Image-ExifTool-$EXIF_VER.tar.gz
 
 if [ ! -d $BIN_PATH ]; then
   mkdir $BIN_PATH
@@ -28,6 +30,6 @@ cd $USER_PATH
 
 ln -sfv $TMP_PATH/ffmpeg-7.0.2-amd64-static/ffmpeg $BIN_PATH/
 ln -sfv $TMP_PATH/ffmpeg-7.0.2-amd64-static/ffprobe $BIN_PATH/
-ln -sfv $TMP_PATH/Image-ExifTool-13.40/exiftool $BIN_PATH/
+ln -sfv $TMP_PATH/Image-ExifTool-$EXIF_VER/exiftool $BIN_PATH/
 
 exit 0
