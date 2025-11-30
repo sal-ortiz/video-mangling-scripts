0e1d54d769a1f82eeb86746edaa7f7979eaeb4bc
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2025-11-30 09:48:45 -0600

    clean up local folders in metadata stript batch script

--- strip_metadata.cmd
+++ strip_metadata.cmd
@@ -3,8 +3,12 @@
 ::setlocal EnableExtensions EnableDelayedExpansion
 ::title scaling down and compressing video
 
-set TMP_PATH=.\tmp
-set BIN_PATH=.\bin
+set USER_PATH=%CD%
+set SCRIPT_PATH=%~dp0
+
+set TMP_PATH=%SCRIPT_PATH%\tmp
+set BIN_PATH=%SCRIPT_PATH%\bin
+
 
 set INP_FILE=%~1
 set OUTP_FILE=%~2
