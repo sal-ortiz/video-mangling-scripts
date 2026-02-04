e2ccc667abbc34f87434de7f54c6610e3ceaba33
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2026-02-03 22:09:54 -0600

    preserve file extension when creating temp files in replace audio shell script

--- replace_audio.sh
+++ replace_audio.sh
@@ -6,25 +6,25 @@ USER_PATH=$( pwd )
 TMP_PATH=$APP_ROOT/tmp
 BIN_PATH=$APP_ROOT/bin
 
-VID_INPUT=$1
-AUD_INPUT=$2
+VID_INP=$1
+AUD_INP=$2
 OUTP_FILE=$3
 
-TMP_FILE=$TMP_PATH/$VID_INPUT.tmp
+TMP_FILE=$TMP_PATH/"${VID_INP%.*}".tmp."${VID_INP##*.}"
 
-if [ -z $VID_INPUT ] || [ -z $AUD_INPUT ]; then
+if [ -z "$VID_INP" ] || [ -z "$AUD_INP" ]; then
   echo "$( basename $0 ) <video> <audio> [output]"
 
   exit 1
 fi
 
-if [ -z $OUTP_FILE ]; then
-  OUTP_FILE=$VID_INPUT
+if [ -z "$OUTP_FILE" ]; then
+  OUTP_FILE=$VID_INP
 fi
 
-cp -fv $VID_INPUT $TMP_FILE
+cp -fv "$VID_INP" "$TMP_FILE"
 
-$BIN_PATH/ffmpeg -y -i $TMP_FILE -i $AUD_INPUT -c:v copy -map 0:v:0 -map 1:a:0 $OUTP_FILE
+$BIN_PATH/ffmpeg -y -i "$TMP_FILE" -i "$AUD_INP" -c:v copy -map 0:v:0 -map 1:a:0 "$OUTP_FILE"
 
 rm -fv $TMP_FILE
 
