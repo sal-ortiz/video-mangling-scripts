8d792f62d065a9f32e50e7be51c7519423a02ce9
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2026-02-03 21:09:11 -0600

    added wav to mp3 conversion batch script

---
+++ wav_to_mp3.cmd
@@ -0,0 +1,26 @@
+@echo off
+
+::setlocal EnableExtensions EnableDelayedExpansion
+::title scaling down and compressing video
+
+set USER_PATH=%CD%
+set SCRIPT_PATH=%~dp0
+
+set BIN_PATH=%SCRIPT_PATH%\bin
+
+set INP_FILE=%~1
+set OUTP_FILE=%~2
+
+
+if "%INP_FILE%" == "" (
+  echo "%0 <input> [output]"
+  exit /b 1
+)
+
+if "%OUTP_FILE%" == "" (
+  set OUTP_FILE=%~n1.mp3
+)
+
+%BIN_PATH%\ffmpeg -y -i "%INP_FILE%" -f mp3 -ab 320k "%OUTP_FILE%"
+
+exit /b 0
