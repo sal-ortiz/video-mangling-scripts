461235e636f38ac7cb3a8ac8e07f82d3cf3a7461
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2026-02-03 22:08:50 -0600

    extract audio to wav instead of mp3 in extract audio shell script

--- extract_audio.sh
+++ extract_audio.sh
@@ -8,16 +8,16 @@ BIN_PATH=$APP_ROOT/bin
 INP_FILE=$1
 OUTP_FILE=$2
 
-if [ -z $INP_FILE ]; then
+if [ -z "$INP_FILE" ]; then
   echo "$( basename $0 ) <input> [output]"
   exit 1
 fi
 
-if [ -z $OUTP_FILE ]; then
-  OUTP_FILE=$INP_FILE.mp3
+if [ -z "$OUTP_FILE" ]; then
+  OUTP_FILE="${INP_FILE%.*}".wav
 fi
 
 # TODO: use -ab <sampling rate> to force output sampling rate.
-$BIN_PATH/ffmpeg -y -i $INP_FILE -f mp3 -vn $OUTP_FILE
+$BIN_PATH/ffmpeg -y -i "$INP_FILE" -f wav -vn "$OUTP_FILE"
 
 exit 0
