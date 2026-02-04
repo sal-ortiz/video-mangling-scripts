a660fe877fb2bb4e8a5d438aaf076a3a1937b52e
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2026-02-03 22:10:54 -0600

    added wav to mp3 conversion shell script

---
+++ wav_to_mp3.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+APP_ROOT=$( readlink --canonicalize $( dirname $0 ) )
+USER_PATH=$( pwd )
+
+TMP_PATH=$APP_ROOT/tmp
+BIN_PATH=$APP_ROOT/bin
+
+INP_FILE=$1
+OUTP_FILE=$2
+
+
+if [ -z "$INP_FILE" ]; then
+  echo "$( basename $0 ) <input> [output]"
+
+  exit 1
+fi
+
+if [ -z "$OUTP_FILE" ]; then
+  OUTP_FILE="${INP_FILE%.*}".mp3
+fi
+
+$BIN_PATH/ffmpeg -y -i "$INP_FILE" -f mp3 -ab 320k "$OUTP_FILE"
+
+exit 0
