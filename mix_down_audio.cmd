a4c1bbb5e5bd0d9bece18dd49f957ad9d89ea51a
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2026-02-05 22:45:57 -0600

    added batch script to mix down multichannel audio

--- mix_down_audio.cmd
+++ mix_down_audio.cmd
@@ -9,24 +9,22 @@ set SCRIPT_PATH=%~dp0
 set TMP_PATH=%SCRIPT_PATH%\tmp
 set BIN_PATH=%SCRIPT_PATH%\bin
 
-
-:: the lower the value the higher the bitrate (24~30 is a good medium)
-set CRF_VAL=%1
-set IMG_HEIGHT=%2
-
+set NUM_CHANNELS=%1
+set VOL_MULTIPLIER=%2
 set INP_FILE=%~3
 set OUTP_FILE=%~4
+
 set TMP_FILE=%TMP_PATH%\%~n3.tmp%~x3
 
-set USAGE_STR="%0 <CRF> <IMG_HEIGHT> <INPUT> [OUTPUT]"
+set USAGE_STR="%0 <channels> <volume> <input> [output]"
 
-if "%CRF_VAL%" == "" (
+if "%NUM_CHANNELS%" == "" (
   echo %USAGE_STR%
 
   exit /b 1
 )
 
-if "%IMG_HEIGHT%" == "" (
+if "%VOL_MULTIPLIER%" == "" (
   echo %USAGE_STR%
 
   exit /b 1
@@ -42,9 +40,8 @@ if "%OUTP_FILE%" == "" set OUTP_FILE=%INP_FILE%
 
 copy /y /v /d "%INP_FILE%" "%TMP_FILE%"
 
-::TODO: try the -af "volume=1.5" switch to adjust volume after mixing down 5.1 audio
-%BIN_PATH%\ffmpeg -y -hwaccel auto -i "%TMP_FILE%" -map_metadata 0 -vcodec libx265 -crf %CRF_VAL% -vf scale=-2:%IMG_HEIGHT% "%OUTP_FILE%"
+%BIN_PATH%\ffmpeg -y -i "%TMP_FILE%" -c:a ac3 -ac %NUM_CHANNELS% -filter:a "volume=%VOL_MULTIPLIER%" "%OUTP_FILE%"
 
-del /q /f "%TMP_FILE%"
+del /s /q /f "%TMP_FILE%"
 
 exit /b 0
