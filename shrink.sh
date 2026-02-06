dd17951418c269a49b461f7e5823b72c2ffbcbdb
Sal Ortiz <this.is.salvador.ortiz@gmail.com>
2026-02-06 12:44:46 -0600

    copy audio stream when shrinking video in shrink bash script

--- shrink.sh
+++ shrink.sh
@@ -32,7 +32,7 @@ TMP_FILE=$TMP_PATH/"${INP_FILE%.*}".tmp."${INP_FILE##*.}"
 cp -fv "$( readlink --canonicalize "$USER_PATH/$INP_FILE" )" "$TMP_FILE"
 
 # TODO: try the "volume=1.5" switch to adjust volume after mixing down 5.1 audio
-"$BIN_PATH/ffmpeg" -y -hwaccel auto -i "$TMP_FILE" -map_metadata 0 -vcodec libx265 -crf $CRF_VAL -ac 2 -vf scale=-2:$IMG_HEIGHT "$( readlink --canonicalize "$USER_PATH/$OUTP_FILE" )"
+"$BIN_PATH/ffmpeg" -y -hwaccel auto -i "$TMP_FILE" -map_metadata 0 -vcodec libx265 -crf $CRF_VAL -vf scale=-2:$IMG_HEIGHT -c:a copy "$( readlink --canonicalize "$USER_PATH/$OUTP_FILE" )"
 
 rm -fv "$TMP_FILE"
 
